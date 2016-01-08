62283cc1a9a067d80f7385757fc4c1b3d779f6cd
valeriy <epsm@i.ua>
2016-01-08 23:00:09 +0200

    DispatcherRunner reworked.

--- src/test/java/com/epsm/electricPowerSystemDispatcher/model/control/RealTimeOperationsRunnerTest.java
+++ src/test/java/com/epsm/electricPowerSystemDispatcher/model/control/RealTimeOperationsRunnerTest.java
@@ -4,8 +4,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class DispatcherRunnerTest{
-	private DispatcherRunner runner = new DispatcherRunner(); 
+public class RealTimeOperationsRunnerTest{
+	private RealTimeOperationsRunner runner = new RealTimeOperationsRunner(); 
 	
 	@Rule
 	public ExpectedException expectedEx = ExpectedException.none();
@@ -13,9 +13,8 @@ public class DispatcherRunnerTest{
 	@Test
 	public void exceptionIfSimulationIsNull(){
 		expectedEx.expect(IllegalArgumentException.class);
-	    expectedEx.expectMessage("DispatcherRunner: dispatcher must not be null.");
+	    expectedEx.expectMessage("RealTimeOperationsRunner: object must not be null.");
 		
 		runner.runDispatcher(null);
 	}
-	
 }
