5fd41a651e2a1fcf1c0ffd94e7884a0517249c87
valeriy <epsm@i.ua>
2016-01-11 00:06:39 +0200

    Clients rewritten.

--- src/main/java/com/epsm/electricPowerSystemDispatcher/client/ConsumerClient.java
+++ src/main/java/com/epsm/electricPowerSystemDispatcher/client/ConsumerClient.java
@@ -1,35 +1,18 @@
 package com.epsm.electricPowerSystemDispatcher.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 
+import com.epsm.electricPowerSystemModel.client.AbstractClient;
 import com.epsm.electricPowerSystemModel.model.consumption.ConsumptionPermissionStub;
-import com.epsm.electricPowerSystemModel.util.UrlRequestSender;
 
-@Import(UrlRequestSender.class)
 @Component
-public class ConsumerClient{
-	
-	@Autowired
-	private UrlRequestSender<ConsumptionPermissionStub> sender;
+public class ConsumerClient extends AbstractClient<ConsumptionPermissionStub>{
 	
 	@Value("${api.consumer.command}")
-	private String consumerApi;
+	private String api;
 	
 	public void sendConsumerPermissionToConsumer(ConsumptionPermissionStub permission){
-		String url = prepareUrl(permission);
-		sendPermission(url, permission);
-	}
-	
-	private String prepareUrl(ConsumptionPermissionStub permission){
-		Long consumerId = permission.getPowerObjectId();
-		
-		return consumerApi + consumerId;
-	}
-	
-	private void sendPermission(String url, ConsumptionPermissionStub permission){
-		sender.sendObjectInJsonToUrlWithPOST(url, permission);
+		sendMessage(permission, api);
 	}
 }
--- src/main/java/com/epsm/electricPowerSystemDispatcher/client/PowerStationClient.java
+++ src/main/java/com/epsm/electricPowerSystemDispatcher/client/PowerStationClient.java
@@ -1,33 +1,18 @@
 package com.epsm.electricPowerSystemDispatcher.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.epsm.electricPowerSystemModel.client.AbstractClient;
 import com.epsm.electricPowerSystemModel.model.generation.PowerStationGenerationSchedule;
-import com.epsm.electricPowerSystemModel.util.UrlRequestSender;
 
 @Component
-public class PowerStationClient{
-	
-	@Autowired
-	private UrlRequestSender<PowerStationGenerationSchedule> sender;
-	
+public class PowerStationClient extends AbstractClient<PowerStationGenerationSchedule>{
+
 	@Value("${api.powerStation.command}")
-	private String powerStationApi;
+	private String api;
 	
 	public void sendGenerationScheduleToPowerStation(PowerStationGenerationSchedule schedule){
-		String url = prepareUrl(schedule);
-		sendSchedule(url, schedule);
-	}
-	
-	private String prepareUrl(PowerStationGenerationSchedule schedule){
-		Long powerStationId = schedule.getPowerObjectId();
-		
-		return powerStationApi + powerStationId;
-	}
-	
-	private void sendSchedule(String url, PowerStationGenerationSchedule schedule){
-		sender.sendObjectInJsonToUrlWithPOST(url, schedule);
+		sendMessage(schedule, api);
 	}
 }
