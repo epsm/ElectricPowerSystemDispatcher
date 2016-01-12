43f9afbffd75e871ac9927230865e4265321e8c5
valeriy <epsm@i.ua>
2016-01-12 11:13:31 +0200

    Logging added to DispatcherFactory

--- src/main/java/com/epsm/electricPowerSystemDispatcher/model/DispatcherFactory.java
+++ src/main/java/com/epsm/electricPowerSystemDispatcher/model/DispatcherFactory.java
@@ -1,5 +1,7 @@
 package com.epsm.electricPowerSystemDispatcher.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -14,17 +16,21 @@ public class DispatcherFactory {
 	private DispatcherImpl dispatcher;
 	private RealTimeOperationsRunner runner;
 	private TimeService timeService;
+	private Logger logger;
 	
 	@Autowired
 	public DispatcherFactory(TimeService timeService){
 		this.timeService = timeService;
 		runner = new RealTimeOperationsRunner();
+		logger = LoggerFactory.getLogger(PowerObjectManagerStub.class);
 	}
 	
 	@Bean
 	public Dispatcher createDispatcher(){
 		createNewDispatcher();
 		runDispatcher();
+		
+		logger.info("DispatcherImpl created and run.");
 		
 		return dispatcher;
 	}
