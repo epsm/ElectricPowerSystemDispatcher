099101791e747c490080e946f3c63f9aa06467a4
valeriy <epsm@i.ua>
2016-01-12 11:12:55 +0200

    ApplicationConfig changed.

--- src/main/java/com/epsm/electricPowerSystemDispatcher/configuration/ApplicationConfig.java
+++ src/main/java/com/epsm/electricPowerSystemDispatcher/configuration/ApplicationConfig.java
@@ -3,12 +3,16 @@ package com.epsm.electricPowerSystemDispatcher.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 
+import com.epsm.electricPowerSystemModel.client.AbstractClient;
+import com.epsm.electricPowerSystemModel.model.generalModel.TimeService;
+
 @Configuration
-@ComponentScan({"com.epsm.electricPowerSystemDispatcher", 
-				"com.epsm.electricPowerSystemModel.model.generalModel"})
+@ComponentScan("com.epsm.electricPowerSystemDispatcher")
+@Import({AbstractClient.class, TimeService.class})			
 public class ApplicationConfig{
 	
 	@Bean
