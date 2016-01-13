a93359f8e915a01fc19aa89b48bd1890a3d77ef5
valeriy <epsm@i.ua>
2016-01-13 12:51:51 +0200

    Packages renamed.

--- src/main/java/com/epsm/electricPowerSystemDispatcher/service/OutgoingMessageService.java
+++
@@ -1,7 +0,0 @@
-package com.epsm.electricPowerSystemDispatcher.service;
-
-import com.epsm.electricPowerSystemModel.model.dispatch.Command;
-
-public interface OutgoingMessageService {
-	void sendCommand(Command command);
-}
--- src/main/java/com/epsm/electricPowerSystemDispatcher/service/PowerObjectService.java
+++
@@ -1,7 +0,0 @@
-package com.epsm.electricPowerSystemDispatcher.service;
-
-import com.epsm.electricPowerSystemModel.model.dispatch.State;
-
-public interface PowerObjectService {
-	void savePowerObjectState(State state);
-}
